87eefbca03ba20c95b51aa7f6c16568c46b18bd4
Just0jo <114908443+Just0jo@users.noreply.github.com>
2024-01-10 22:30:08 +0000

    song service

---
+++ src/main/java/ie/atu/project1_c/SongService.java
@@ -0,0 +1,18 @@
+package ie.atu.project1_c;
+
+import lombok.Data;
+import org.springframework.stereotype.Service;
+@Service
+@Data
+public class SongService {
+    public String ackMessage(SongUser info_2)
+    {
+        String message = "Here are the songs from Charlie puth album Night Track Mind";
+        return message;
+
+    }
+
+
+}
+
+
